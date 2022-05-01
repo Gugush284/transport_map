5f92737b24a483c58900021828af0961b49c0da0
DmMkh <95351881+DmMkh@users.noreply.github.com>
2022-05-01 18:58:23 +0300

    add autocomplete function

--- server/app/views.py
+++ server/app/views.py
@@ -1,33 +1,54 @@
-from flask import render_template, flash, redirect, request
+from flask import render_template, flash, redirect, request, Response
+from flask.wrappers import Request
 from app import app
-from app import forms
+import json
+from wtforms import TextField, SubmitField, Form
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
+class SearchForm(FlaskForm):
+    autocomp = TextField('Station 1', id = 'city_autocomplete', validators = [DataRequired()])
+    autocomp1 = TextField('Station 2', id = 'ity_autocomplete', validators = [DataRequired()])
+    submit = SubmitField()
 
-@app.route('/', methods=["POST", "GET"] )
-@app.route('/index', methods=["POST", "GET"])
-@app.route("/login", methods=["POST", "GET"])
-# added URL routes
+cities = ['']
+f = open("E:/databasa.txt", "r", encoding='utf-8')
+i = 0
+k = 0
+name = "" 
+buf = ""
+N = 0   
+x = 1.1
+y = 1.1
+points = []
+names = []
+for line in f:
+    N = N + 1
+    i = 0
+    while(line[i] != '3' or line[i+1] != '7' or line[i+2] != '.'):
+        name = name + line[i]
+        i = i + 1
+    name = name[:len(name)-1]
+    names.append(name)
+    name = ''  
+cities = names
+@app.route('/_autocomplete', methods=['GET'])
+def autocomplete(): 
+    return Response(json.dumps(cities), mimetype='application/json')
 
-def login():
-    bus1 = ''
-    # bus station user typed in form
-    bus2 = ''
-    # bus station user typed in form
-    message = ''
-    # message of handling bus1 and bus2
-    if request.method == 'GET':
-        message = "Enter staion names"
-    if request.method == 'POST':
-	    bus1 = request.form.get('Station1')
-	    bus2 = request.form.get('Station2')
-    #form request handling
-    if (len(bus1) < 1 or len(bus1) > 100):
-        message = "bad data"
-    if (len(bus2) < 1 or len(bus2) > 100):
-        message = "bad data"
-    # simple test of entered data
-    return render_template('login.html', message=message)
-# function - handler of '/login' request
 
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
-# function - handler of '/index' request
+    bus1 = ''
+    bus2 = ''
+    form = SearchForm(request.form)
+    if form.validate_on_submit():
+        bus1 = form.autocomp.data
+        bus2 = form.autocomp1.data
+        print(bus1)
+        print(bus2)
+        return render_template("search.html", form=form)
+    return render_template("search.html", form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")

12a44d463624d649af633344f01fc5d12c0f94d4
Gugush284 <gugush284@gmail.com>
2022-05-08 14:53:21 +0300

    Rewrite sql queries

--- server/reading/reading.py
+++ server/reading/reading.py
@@ -26,26 +26,28 @@ def read_stops(sql_connection, cur):
 
     try:
         # Get all stop ids
-        command = "SELECT _id FROM stopsker"
-        cur.execute(command)
+        cur.execute("SELECT _id FROM stopsker")
         amount = cur.fetchall()
 
         # For each id read indo from data base to class stop
         for item in amount:
-            command = "SELECT Name_stop FROM stopsker WHERE _id = "
-            command += str(item[0])
-            cur.execute(command)
+            cur.execute("SELECT Name_stop FROM stopsker WHERE _id = ?",
+                [item[0]])
 
             name = str(cur.fetchone()[0])
 
-            command = "SELECT Cords FROM stopsker WHERE _id = "
-            command += str(item[0])
-            cur.execute(command)
+            cur.execute("SELECT Cords FROM stopsker WHERE _id = ?", 
+                [item[0]])
 
             coords = cur.fetchone()[0].split()
 
-            stops.append(STOP(int(item[0]), name, float(coords[0]),
-                float(coords[1])))
+            stops.append(STOP(
+                int(item[0]),
+                name,
+                float(coords[0]),
+                float(coords[1])
+                )
+            )
 
     except Exception as e:
         print({e})
@@ -62,10 +64,8 @@ def read_optimal_route(stop1, stop2, sql, cur):
         # For two stops get optimal route between them
 
         # Get id of stop1
-        command = "SELECT _id FROM stopsker WHERE Name_stop = '"
-        command += str(stop1)
-        command += "'"
-        cur.execute(command)
+        cur.execute("SELECT _id FROM stopsker WHERE Name_stop = ?",
+            [stop1])
 
         Input = cur.fetchone()
 
@@ -76,10 +76,8 @@ def read_optimal_route(stop1, stop2, sql, cur):
             id1 = int(Input[0])
 
         # Get id of stop2
-        command = "SELECT _id FROM stopsker WHERE Name_stop = '"
-        command += str(stop2)
-        command += "'"
-        cur.execute(command)
+        cur.execute("SELECT _id FROM stopsker WHERE Name_stop = ?",
+            [stop2])
 
         Input = cur.fetchone()
 
@@ -90,41 +88,31 @@ def read_optimal_route(stop1, stop2, sql, cur):
             id2 = int(Input[0])
 
         # Get optimal way and transfers in it  
-        command = "SELECT route, transfer FROM way WHERE id1 = "
-        command += str(id1)
-        command += " and id2 = "
-        command += str(id2)
-        cur.execute(command)
+        cur.execute("SELECT route, transfer FROM way WHERE id1 = ? and id2 = ?",
+        [id1, id2])
         Input = cur.fetchone()
 
         # if no way 
         if Input is None:
             return -3, [], []
 
-        # str route -> int way
-        # way consists of ids
-        way = [int(elem) for elem in Input[0].split()]
-
         # way_str consists of names
         way_str = list()
-        for elem in way:
-            command = "SELECT Name_stop FROM stopsker WHERE _id = "
-            command += str(elem)
-            cur.execute(command)
+        for elem in Input[0].split():
+            cur.execute("SELECT Name_stop FROM stopsker WHERE _id = ?",
+                [elem])
             way_str.append(cur.fetchone()[0])
 
         transfer = list()
         for elem in Input[1].split(";"):
             one_transfer = elem.split()
             
-            command = "SELECT Name_stop FROM stopsker WHERE _id = "
-            command += str(one_transfer[0])
-            cur.execute(command)
+            cur.execute("SELECT Name_stop FROM stopsker WHERE _id = ?",
+            one_transfer[0])
             name_stop = cur.fetchone()[0]
 
-            command = "SELECT Name_route FROM routesker WHERE _id = "
-            command += str(one_transfer[1])
-            cur.execute(command)
+            cur.execute("SELECT Name_route FROM routesker WHERE _id = ?",
+            [one_transfer[1]])
             name_route = cur.fetchone()[0]
             
             transfer.append([name_stop, name_route])
@@ -154,8 +142,11 @@ def read_db(stop1, stop2, sql_connection, cursor):
 
 def connection():
     try:
-        path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-        'example_server.db')
+        path = os.path.join(
+            os.path.dirname(
+                os.path.abspath(__file__)),
+            'example_server.db'
+        )
         sqlite_connection = sqlite3.connect(path)
         cur = sqlite_connection.cursor()
 
@@ -164,21 +155,6 @@ def connection():
         exit()
     else:
         return sqlite_connection, cur
-
-def fun():
-    sql_connection, cursor = connection()
-
-    print("Enter first stop")
-    stop1 = "STOP 1"
-    print("Enter second stop")
-    stop2 = "STOP 7"
-
-    # stops is a list of class STOP elements
-    # way is list of stops in route order
-    # transfer is list of [name of stop, name of route] 
-    stops, way, transfer = read_db(stop1, stop2, sql_connection, cursor)
-
-    return stops, way, transfer
 
 def TEST_PRINT_STOPS(stops, way, transfer):
     print("\nOptimal way:")
@@ -192,11 +168,16 @@ def TEST_PRINT_STOPS(stops, way, transfer):
         print("{}) {} - {} {}".format(stop.get_id(),
         stop.get_name(), stop.get_x(), stop.get_y()))
 
-def main():
-    stops, way, transfer = fun()
-    TEST_PRINT_STOPS(stops, way, transfer)
+def read(stop1, stop2):
+    sql_connection, cursor = connection()
+
+    # stops is a list of class STOP elements
+    # way is list of stops in route order
+    # transfer is list of [name of stop, name of route] 
+    stops, way, transfer = read_db(stop1, stop2, sql_connection, cursor)
+
+    return stops, way, transfer
 
 if __name__ == "__main__":
-    main()
-else:
-    fun() 
+    stops, way, transfer = read("STOP 7", "STOP 9")
+    TEST_PRINT_STOPS(stops, way, transfer)
